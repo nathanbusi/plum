f4861f2e252ea2bd9a12bff7bafe9d755298b231
nathanbusi <nathan.busi00@gmail.com>
2025-04-05 19:04:32 -0400

    sim is working with ros2 control and publishing accurately to rviz about to try getting it working w real robot. this is where the code starts using commands to swap between sim and real after this push.

--- src/plum/launch/sim_launch.launch.py
+++ src/plum/launch/sim_launch.launch.py
@@ -34,8 +34,21 @@ def generate_launch_description():
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'plum'],
+                                   '-entity', 'my_bot'],
                         output='screen')
+
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
 
 
 
@@ -44,4 +57,6 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
